3a4d9e97e208831ca07f5c4f408177fc88c10a06
Thiago Osorio <thiagoq33@hotmail.com>
2025-10-20 09:27:41 -0300

    Creating API code

---
+++ api/exceptions.py
@@ -0,0 +1,11 @@
+class ModelLoadError(Exception):
+    pass
+
+class FeaturesLoadError(Exception):
+    pass
+
+class DataFrameLoadError(Exception):
+    pass
+
+class PredictionError(Exception):
+    pass
